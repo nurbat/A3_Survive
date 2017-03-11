34758c08370b7a489b4da21740a8a7555da70110
nurbat <hummer_4x4@list.ru>
2017-03-11 12:24:19 +0200

    Add files via upload

---
+++ @SRV_Server/addons/player.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 11, 2017 at 01:24 PM
+-- Server version: 5.6.33
+-- PHP Version: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `u0103425_arma_server_1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player`
+--
+
+CREATE TABLE `player` (
+  `uid` varchar(32) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `money` int(11) UNSIGNED NOT NULL DEFAULT '0',
+  `damage` double UNSIGNED NOT NULL DEFAULT '0',
+  `hitpoints` varchar(255) NOT NULL DEFAULT '[]',
+  `direction` double NOT NULL DEFAULT '0',
+  `position_x` double NOT NULL DEFAULT '0',
+  `position_y` double NOT NULL DEFAULT '0',
+  `position_z` double NOT NULL DEFAULT '0',
+  `spawned_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `assigned_items` text NOT NULL,
+  `backpack` varchar(64) NOT NULL,
+  `backpack_items` text NOT NULL,
+  `backpack_magazines` text NOT NULL,
+  `backpack_weapons` text NOT NULL,
+  `current_weapon` varchar(64) NOT NULL,
+  `goggles` varchar(64) NOT NULL,
+  `handgun_items` text NOT NULL,
+  `handgun_weapon` varchar(64) NOT NULL,
+  `headgear` varchar(64) NOT NULL,
+  `binocular` varchar(64) NOT NULL,
+  `loaded_magazines` text NOT NULL,
+  `primary_weapon` varchar(64) NOT NULL,
+  `primary_weapon_items` text NOT NULL,
+  `secondary_weapon` varchar(64) NOT NULL,
+  `secondary_weapon_items` text NOT NULL,
+  `uniform` varchar(64) NOT NULL,
+  `uniform_items` text NOT NULL,
+  `uniform_magazines` text NOT NULL,
+  `uniform_weapons` text NOT NULL,
+  `vest` varchar(64) NOT NULL,
+  `vest_items` text NOT NULL,
+  `vest_magazines` text NOT NULL,
+  `vest_weapons` text NOT NULL,
+  `last_updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
